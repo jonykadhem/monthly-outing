678844e04a3be86371085e259b9baf02c1f6167c
unknown <jonykadhem@gmail.com>
2026-07-19 10:37:00 +0300

    set up

---
+++ controllers/auth.js
@@ -0,0 +1,98 @@
+﻿const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+const home = (req, res) => {
+    res.render("home.ejs", {
+        user: req.session.user,
+    });
+};
+
+const showSignUpForm = (req, res) => {
+    res.render("auth/sign-up.ejs", {
+        user: req.session.user,
+    });
+};
+
+const signUp = async (req, res) => {
+    const userInDatabase = await User.findOne({
+        username: req.body.username,
+    });
+
+    if (userInDatabase) {
+        return res.send("Username is already taken");
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const userData = {
+        username: req.body.username,
+        password: hashedPassword,
+    };
+
+    const user = await User.create(userData);
+
+    req.session.user = {
+        username: user.username,
+        id: user.id,
+    };
+
+    req.session.save(() => {
+        res.redirect("/");
+    });
+};
+
+const showSignInForm = (req, res) => {
+    res.render("auth/sign-in.ejs", {
+        user: req.session.user,
+    });
+};
+
+const signIn = async (req, res) => {
+    const userInDatabase = await User.findOne({
+        username: req.body.username,
+    });
+
+    if (!userInDatabase) {
+        return res.send("User does not exist");
+    }
+
+    const validPassword = await bcrypt.compare(
+        req.body.password,
+        userInDatabase.password
+    );
+
+    if (!validPassword) {
+        return res.send("Login failed");
+    }
+
+    req.session.user = {
+        username: userInDatabase.username,
+        id: userInDatabase.id,
+    };
+
+    req.session.save(() => {
+        res.redirect("/");
+    });
+};
+
+const signOut = (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/");
+    });
+};
+
+const dashboard = (req, res) => {
+    res.render("dashboard.ejs", {
+        user: req.session.user,
+    });
+};
+
+module.exports = {
+    home,
+    showSignUpForm,
+    signUp,
+    showSignInForm,
+    signIn,
+    signOut,
+    dashboard,
+};
---
+++ server.js
@@ -0,0 +1,66 @@
+﻿const dns = require("node:dns");
+
+// DNS workaround for MongoDB Atlas.
+// Remove these two lines if your regular DNS works correctly.
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+require("dotenv").config();
+
+const authCtrl = require("./controllers/auth.js");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+const methodOverride = require("method-override");
+const { MongoStore } = require("connect-mongo");
+const upload = require("./config/multer");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000; // For me, this is best practice. Usually, the port is placed in .env; if not, 3000 will be used
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI,
+        }),
+    })
+);
+
+app.use(passUserToView);
+
+app.get("/", authCtrl.home);
+app.get("/auth/sign-up", authCtrl.showSignUpForm);
+app.post("/auth/sign-up", authCtrl.signUp);
+app.get("/auth/sign-in", authCtrl.showSignInForm);
+app.post("/auth/sign-in", authCtrl.signIn);
+app.delete("/auth/sign-out", authCtrl.signOut);
+
+app.get("/dashboard", isSignedIn, authCtrl.dashboard);
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
+
+        app.listen(PORT, () => {
+            console.log(`Listening on ${PORT}`);
+        });
+    } catch (error) {
+        console.log("MongoDB connection error:", error.message);
+    }
+};
+
+startServer();
